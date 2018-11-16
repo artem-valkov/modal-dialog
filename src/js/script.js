864b84a88e2ba25ddd7690dc1676a9ddcc77a8b1
Artem Valkov <valkov.artyom@gmail.com>
2018-11-16 16:13:45 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -5,22 +5,19 @@
   const closeBtns = document.querySelectorAll('[data-hide]');
   const uninstall = document.querySelector('.ModalPage__content--uninstall');
   button.addEventListener('click', (event) => {
+    event.preventDefault();
     content.classList.remove('hide');
     back.classList.add('blackbg')});
 
-    for (let i = 0; i < closeBtns.length; i++)
-    closeBtns[i].addEventListener('click', (event) => {
-      content.classList.add('hide');
-      back.classList.remove('blackbg')});
-
-    back.addEventListener('click', function (event) {
-      if(event.target == back) {
+  for (let i = 0; i < closeBtns.length; i++)
+    closeBtns[i].addEventListener('click', function (event) {
+       if(event.target == closeBtns[i]) {
         content.classList.add('hide');
         back.classList.remove('blackbg');
-      }});
-    uninstall.addEventListener('click', (event) => {
-      event.preventDefault();
-      alert('hi');
-      content.classList.add('hide');
-      back.classList.remove('blackbg')});
+      } else if (event.target == uninstall) {
+        alert('hi');
+        content.classList.add('hide');
+        back.classList.remove('blackbg');
+      }
+    });
 })();
