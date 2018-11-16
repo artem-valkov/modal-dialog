8fd524cb579a32dc60753f99698a51c866db2984
Artem Valkov <valkov.artyom@gmail.com>
2018-11-16 17:29:00 +0200

    fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,13 +9,14 @@
     content.classList.remove('hide');
     back.classList.add('blackbg')});
 
+
   for (let i = 0; i < closeBtns.length; i++)
     closeBtns[i].addEventListener('click', function (event) {
        if(event.target == closeBtns[i]) {
         content.classList.add('hide');
         back.classList.remove('blackbg');
       } else if (event.target == uninstall) {
-        alert('hi');
+         (setTimeout("alert('hi')" ,200));
         content.classList.add('hide');
         back.classList.remove('blackbg');
       }
