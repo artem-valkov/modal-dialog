51f7b51442fe64599a847b7fcdd442b39893655c
Artem Valkov <valkov.artyom@gmail.com>
2018-11-21 13:54:27 +0200

    done

--- dist/js/script2.js
+++ dist/js/script2.js
@@ -1,25 +1,41 @@
 (function () {
+  const pageModal = document.querySelectorAll('[data-page]');
   const runModal = document.querySelectorAll('[data-run]');
   const contentModal = document.querySelectorAll('[data-content]');
   const primaryBtn = document.querySelectorAll('[data-primarybtn]');
-  const secondaryBtn = document.querySelectorAll('[date-secondarybtn]');
+  const secondaryBtn = document.querySelectorAll('[data-secondarybtn]');
+  const mainImage = document.querySelectorAll('[data-image]');
+  const overlay = document.createElement('div');
+  overlay.className = 'overlay';
+  const closeBtn = document.createElement('a');
+  closeBtn.className = 'ModalPage__close';
+  closeBtn.innerHTML = '<img src="images/icon-popup-close.svg">';
 
-  function contentShow(button) {
-    button.addEventListener('click', (event) => {
-      event.preventDefault();
-      for (let c = 0; c < contentModal.length; c++) {
-        if (button.dataset.run === contentModal[c].dataset.content) {
-          contentModal[c].classList.remove('hide');
-        }
-      }
+  for (let m = 0; m < runModal.length; m++) {
+    const modal = contentModal[m];
+    const page = pageModal[m];
+    const run = runModal[m];
+    const windowModal = function (modal, page) {
+      modal.classList.remove('hide');
+      page.appendChild(overlay);
+      modal.appendChild(closeBtn);
+    };
+    runModal[m].addEventListener('click', () => {
+      windowModal(modal, page);
     });
+    const closeModal = function (modal, page) {
+      modal.classList.add('hide');
+      page.removeChild(overlay);
+    };
+    primaryBtn[m].addEventListener('click', () => {
+      closeModal(modal, page);
+      (setTimeout("alert('hi')", 250));
+    });
+    secondaryBtn[m].addEventListener('click', () => {
+      closeModal(modal, page);
+    });
+    closeBtn.addEventListener('click', () => {
+      closeModal(modal, page);
+    })
   }
-  function showModal() {
-    for (let r = 0; r < runModal.length; r++) {
-      const button = runModal[r];
-      contentShow(button);
-    }
-  }
-
-  showModal();
 }());
